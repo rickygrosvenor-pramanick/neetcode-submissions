43893ce85f7c9a75a4bc33f744df069c171aec43
ricky pramanick <107711090+rickygrosvenor-pramanick@users.noreply.github.com>
2026-04-18 11:34:36 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        num_open, num_closed = 0, 0
+        num_curly_open, num_curly_closed = 0, 0
+        num_square_open, num_square_closed = 0, 0
+
+        stack = []
+        for i in range(len(s)):
+            if s[i] == "(":
+                num_open += 1
+                stack.append(s[i])
+            elif s[i] == ")":
+                num_closed += 1
+                if len(stack) == 0 or stack[-1] != "(":
+                    return False
+                else:
+                    stack.pop()
+            elif s[i] == "{":
+                num_curly_open += 1
+                stack.append(s[i])
+            elif s[i] == "}":
+                num_curly_closed += 1
+                if len(stack) == 0 or stack[-1] != "{":
+                    return False
+                else:
+                    stack.pop()
+            elif s[i] == "[":
+                num_square_open += 1
+                stack.append(s[i])
+            elif s[i] == "]":
+                num_square_closed += 1
+                if len(stack) == 0 or stack[-1] != "[":
+                    return False
+                else:
+                    stack.pop()
+            
+            if num_closed > num_open or num_curly_closed > num_curly_open or num_square_closed > num_square_open:
+                return False
+        
+        if num_closed != num_open or num_curly_closed != num_curly_open or num_square_closed != num_square_open:
+            return False
+        
+        return True
+            
+            
