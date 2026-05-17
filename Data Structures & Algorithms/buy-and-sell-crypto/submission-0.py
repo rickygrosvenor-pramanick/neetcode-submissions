19af95560fba5cc1cec89dcf2f3660ec9c2bd8f8
ricky pramanick <107711090+rickygrosvenor-pramanick@users.noreply.github.com>
2026-05-17 17:38:35 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left_window = 101
+        right_window = 101
+        max_profit_so_far = 0
+
+        for i in range(len(prices)):
+            if left_window > prices[i]:
+                left_window = prices[i]
+                right_window = prices[i]
+
+            if right_window > left_window:
+                right_window = max(prices[i], right_window)
+            
+            
+            max_profit_so_far = max(max_profit_so_far, right_window - left_window)
+        
+        return max_profit_so_far
