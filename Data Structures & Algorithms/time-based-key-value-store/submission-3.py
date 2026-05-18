6b7c9716423941b4a5181028fa9d008793fe49fd
ricky pramanick <107711090+rickygrosvenor-pramanick@users.noreply.github.com>
2026-05-18 13:59:39 -0400

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,39 @@
+class TimeMap:
+
+    def __init__(self):
+        self.map = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.map:
+            self.map[key] = [(timestamp, value)]
+        else:
+            self.map[key].append((timestamp, value))
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.map:
+            return ""
+
+        array_to_scan = self.map[key]
+
+        def iterative_search(a, b):
+
+            while a <= b:
+                mid = (a + b) // 2
+
+                if array_to_scan[mid][0] == timestamp:
+                    return array_to_scan[mid][1]
+                
+                if array_to_scan[mid][0] > timestamp:
+                    b = mid - 1
+                else:
+                    a = mid + 1
+            
+            if b >= 0 and array_to_scan[b][0] < timestamp:
+                return array_to_scan[b][1]
+            else:
+                return ""
+
+        return iterative_search(0, len(array_to_scan) - 1)
+                
+        
