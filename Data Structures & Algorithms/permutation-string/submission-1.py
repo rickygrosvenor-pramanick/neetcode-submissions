ada22d9a520cb6687e94888bfe9d7960e71d3789
ricky pramanick <107711090+rickygrosvenor-pramanick@users.noreply.github.com>
2026-05-24 01:39:08 -0400

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq_vector_s1 = [0] * 26
+        for c in s1:
+            freq_vector_s1[ord(c) - ord('a')] += 1
+        
+        for i in range(0, len(s2) - len(s1)):
+            freq_vector_curr = [0] * 26
+            current_block = s2[i: i + len(s1)]
+            for c in current_block:
+                freq_vector_curr[ord(c) - ord('a')] += 1
+
+            equal = True
+            for i in range(0, 26):
+                if freq_vector_s1[i] != freq_vector_curr[i]:
+                    equal = False
+            
+            if equal is True:
+                return True
+        
+        return False
+
