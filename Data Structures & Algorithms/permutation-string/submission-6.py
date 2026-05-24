5179e5902a9e8d28af8d2119b55ef9154c19eaa2
ricky pramanick <107711090+rickygrosvenor-pramanick@users.noreply.github.com>
2026-05-24 02:02:12 -0400

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq_vector_s1 = [0] * 26
+        window_vector = [0] * 26
+        for i in range(len(s1)):
+            freq_vector_s1[ord(s1[i]) - ord('a')] += 1
+            window_vector[ord(s2[i]) - ord('a')] += 1
+        
+        if freq_vector_s1 == window_vector:
+            return True
+        
+        for i in range(len(s1), len(s2) - len(s1) + 1):
+            window_vector[ord(s2[i - len(s1)]) - ord('a')] -= 1
+            window_vector[ord(s2[i]) - ord('a')] += 1
+            if freq_vector_s1 == window_vector:
+                return True
+        
+        return False
+
